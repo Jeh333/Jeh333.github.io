435494abcda7349447579a980710be528f75f51b
Jeh333 <jeh333@umsystem.edu>
2025-02-25 21:12:58 -0600

    Updated server.js

--- backend/server.js
+++ backend/server.js
@@ -1,22 +1,20 @@
-//command to start:<node server.js>
-
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+require("dotenv").config();
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors());
 app.use(bodyParser.json());
 
 // MongoDB Connection
+const MONGO_URI = process.env.MONGO_URI;
 mongoose
-  .connect("mongodb://localhost:27017/Jeh333.github.io", {
-    useNewUrlParser: true,
-  })
+  .connect(MONGO_URI)
   .then(() => console.log("MongoDB Connected"))
   .catch((err) => console.log(err));
 
@@ -31,13 +29,17 @@ const Form = mongoose.model("Form", FormSchema);
 
 // Routes
 app.post("/submit", async (req, res) => {
-  const { name, email, message } = req.body;
-  const newForm = new Form({ name, email, message });
-  await newForm.save();
-  res.status(201).json({ message: "Form submitted successfully!" });
+  try {
+    const { name, email, message } = req.body;
+    const newForm = new Form({ name, email, message });
+    await newForm.save();
+    res.status(201).json({ message: "Form submitted successfully!" });
+  } catch (err) {
+    res.status(500).json({ error: "Internal server error" });
+  }
 });
 
 // Start the server
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
