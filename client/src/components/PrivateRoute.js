831d2375c344e90c42471143d73a844d355d62e7
Jeh333 <116123209+Jeh333@users.noreply.github.com>
2025-03-20 19:02:46 -0500

    Delete client directory

--- client/src/components/PrivateRoute.js
+++
@@ -1,8 +0,0 @@
-import { Navigate } from "react-router-dom";
-
-function PrivateRoute({ element }) {
-  const token = localStorage.getItem("token"); // Check if user is authenticated
-  return token ? element : <Navigate to="/login" />;
-}
-
-export default PrivateRoute;
